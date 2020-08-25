e1db27e9a94ed0b97dd70c77fbd84d0af96230c7
Lucas Weber <lucas.weber@delinea.com.br>
2020-08-25 18:16:39 -0300

    created_datetime formated on PostSerializer

--- careers/serializers.py
+++ careers/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from careers.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    created_datetime = serializers.DateTimeField(format="%d/%m/%Y - %H:%M:%S")
+
     class Meta:
         model = Post
         fields = (
