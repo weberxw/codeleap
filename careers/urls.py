e8f44f404e700298338be5a136523ecaef6c6bf4
Lucas Weber <lucas.weber@delinea.com.br>
2020-08-26 10:17:04 -0300

    removed app name from url

--- careers/urls.py
+++ careers/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from careers import views
 
 urlpatterns = [
-    url(r'posts/(?P<pk>[0-9]+)/', views.Posts.as_view()),
-    url(r'posts/', views.Posts.as_view()),
+    url(r'(?P<pk>[0-9]+)/', views.Posts.as_view()),
+    url(r'', views.Posts.as_view()),
 ]
